aba92f785f7529eb43614a25e8e0e9cc7807c657
yuripourre <yuriamayuri@gmail.com>
2014-02-21 12:00:01 -0300

    Removing old Posit Modifier

--- Etyllic Motion/src/br/com/etyllica/motion/filter/modifier/posit/TCamera.java
+++
@@ -1,9 +0,0 @@
-package br.com.etyllica.motion.filter.modifier.posit;
-
-public class TCamera {
-
-	int focalLength;
-	double[][] rotation = new double[3][3];/* Rotation of SCENE in camera reference, NOT other way around */
-	double[] translation = new double[3];/* Translation of SCENE in camera reference */
-	
-}
