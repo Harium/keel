9afa9d74d20eab35365d5a1c81474e23825675c1
Yuri Pourre <yuriamayuri@gmail.com>
2014-02-22 18:43:31 -0300

    BugFix CoplanarPosit calculate rotations from points in different order

--- Etyllic Motion/src/br/com/etyllica/motion/filter/modifier/PositCoplanarModifier.java
+++ Etyllic Motion/src/br/com/etyllica/motion/filter/modifier/PositCoplanarModifier.java
@@ -1,5 +1,9 @@
 package br.com.etyllica.motion.filter.modifier;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.etyllica.linear.Point2D;
 import br.com.etyllica.motion.core.helper.RotationAxis;
 import br.com.etyllica.motion.core.strategy.ComponentModifierStrategy;
 import br.com.etyllica.motion.features.Component;
@@ -15,16 +19,29 @@ public class PositCoplanarModifier extends RotationAxis implements ComponentModi
 		
 	private CoplanarPosit posit;
 	
+	private List<Point2D> imagePoints;
+	
 	public PositCoplanarModifier() {
 		super();
 		
 		posit = new CoplanarPosit(1, 760);
+		
+		imagePoints = new ArrayList<Point2D>();
 	}
 
 	@Override
 	public Component modifyComponent(Component component) {
+				
+		List<Point2D> points = component.getPoints();
 		
-		Pose pose = posit.pose(component.getPoints());
+		imagePoints.clear();
+		
+		imagePoints.add(points.get(0));
+		imagePoints.add(points.get(1));
+		imagePoints.add(points.get(3));
+		imagePoints.add(points.get(2));
+		
+		Pose pose = posit.pose(imagePoints);
 		
 		this.error = pose.getBestError();
 		
