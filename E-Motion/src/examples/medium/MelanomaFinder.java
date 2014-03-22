54fe2602e6f19659cc308ec0d81d7649361b72ec
Yuri Pourre <yuriamayuri@gmail.com>
2014-03-22 13:13:12 -0300

    Add main method in MelanomaFinder class

--- E-Motion/src/examples/medium/MelanomaFinder.java
+++ E-Motion/src/examples/medium/MelanomaFinder.java
@@ -1,16 +1,23 @@
 package examples.medium;
 
 
-import br.com.etyllica.Etyllica;
+import br.com.etyllica.EtyllicaFrame;
 import br.com.etyllica.context.Application;
 import examples.medium.application.MelanomaFinderApplication;
 
-public class MelanomaFinder extends Etyllica {
+public class MelanomaFinder extends EtyllicaFrame {
 
 	private static final long serialVersionUID = 1L;
 
 	public MelanomaFinder() {
 		super(640, 480);
+	}
+	
+	public static void main(String[] args) {
+		
+		MelanomaFinder finder = new MelanomaFinder();
+		finder.init();	
+		
 	}
 
 	@Override
@@ -20,7 +27,7 @@ public class MelanomaFinder extends Etyllica {
 				
 		setPath(s+"../../");
 		
-		return new MelanomaFinderApplication(w,h);		
+		return new MelanomaFinderApplication(w,h);
 		
 	}	
 
