e69f504dff0c8f4ed82eb37158c92b48a12c89b8
yuripourre <yuriamayuri@gmail.com>
2014-03-28 15:50:11 -0300

    Add neighbor validation in FloodFillSearch

--- E-Motion/src/br/com/etyllica/motion/filter/search/FloodFillSearch.java
+++ E-Motion/src/br/com/etyllica/motion/filter/search/FloodFillSearch.java
@@ -14,63 +14,67 @@ import br.com.etyllica.motion.filter.validation.CountComponentPoints;
 public class FloodFillSearch extends BooleanMaskSearch {
 
 	private Component lastComponent;
-	
+
+	private int minNeighbors = 1;
+
 	public FloodFillSearch(int w, int h) {
 		super(w, h, new SkinColorStrategy());
-		
+
 		this.validations.add(new CountComponentPoints(180));
 	}
-	
+
 	@Override
 	public Component filterFirst(BufferedImage bimg, Component component) {
 
 		List<Component> list = filter(bimg, component);
-		
+
 		if(!list.isEmpty()){
-			
+
 			lastComponent = list.get(0);
-			
+
 		}
-		
+
 		return lastComponent;
-		
+
 	}
 
 	@Override
 	public List<Component> filter(BufferedImage bimg, Component component) {
 
 		super.setup();
-		
+
 		int x = border;
-		
+
 		int y = border;
-		
+
 		int width = component.getW()-border*2;
-		
+
 		int height = component.getH()-border*2;
-		
+
 		for (int j = y; j < height; j+=step) {
 
 			for (int i = x; i < width; i+=step) {
 
 				if (!mask[i][j] && pixelStrategy.validateColor(bimg.getRGB(i,j))) {
-										
+
 					Queue<Point2D> queue = new LinkedList<Point2D>();
-					
+
 					queue.add(new Point2D(i, j));
 
 					Component found = new Component();
 
 					while (!queue.isEmpty()) {
-						
+
 						Point2D p = queue.remove();
 
-						if ((p.getX() >= x) && (p.getX() < width &&
-								(p.getY() >= y) && (p.getY() < height))) {
-							
-							if (!mask[(int)p.getX()][(int)p.getY()] && 
-									pixelStrategy.validateColor(bimg.getRGB((int)p.getX(), (int)p.getY()))) {
-								
+						int px = (int)p.getX();
+						int py = (int)p.getY();
+
+						if ((px >= x) && (px < width &&
+								(py >= y) && (py < height))) {
+
+							if (verifyPixel(px, py, bimg)) {
+
 								mask[(int)p.getX()][(int)p.getY()] = true;
 
 								found.add(p);
@@ -81,9 +85,9 @@ public class FloodFillSearch extends BooleanMaskSearch {
 								queue.add(new Point2D((int)p.getX(), (int)p.getY() - step));
 
 							}
-							
+
 						}
-						
+
 					}
 
 					if(this.validate(found)) {
@@ -93,15 +97,52 @@ public class FloodFillSearch extends BooleanMaskSearch {
 					}
 
 				} else {
-					
+
 					mask[i][j] = true;
-					
+
 				}
-				
+
 			}
 		}
 
 		return result;
+	}
+	
+	private boolean verifyPixel(int px, int py, BufferedImage bimg) {
+
+		if (verifySinglePixel(px, py, bimg)) {
+
+			return (minNeighbors<=1)||verifyNeighbors(px, py, bimg);
+
+		}
+
+		return false;
+
+	}
+
+	private boolean verifySinglePixel(int px, int py, BufferedImage bimg) {
+
+		return (!mask[px][py] && pixelStrategy.validateColor(bimg.getRGB(px, py)));
+
+	}
+
+	private boolean verifyNeighbors(int px, int py, BufferedImage bimg) {
+		
+		int verified = 0;
+		
+		for(int j=py-1; j<py+1; j++) {
+				
+			for(int i=px-1; i<px+1; i++) {
+				
+				if(pixelStrategy.validateColor(bimg.getRGB(i, j))) {
+					verified++;
+				}
+				
+			}
+		
+		}		
+				
+		return verified >= minNeighbors;
 	}
 
 }
