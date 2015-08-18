c105725f7163335d940c39f662d524b850149ae7
Yuri Pourre <yuriamayuri@gmail.com>
2015-08-18 11:16:25 -0300

    Improves OGR example

--- src/main/java/examples/medium/ogr/OGRApplication.java
+++ src/main/java/examples/medium/ogr/OGRApplication.java
@@ -3,7 +3,11 @@ package examples.medium.ogr;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.dyn4j.geometry.Edge;
 
 import br.com.etyllica.core.context.Application;
 import br.com.etyllica.core.event.GUIEvent;
@@ -11,24 +15,31 @@ import br.com.etyllica.core.event.MouseButton;
 import br.com.etyllica.core.event.PointerEvent;
 import br.com.etyllica.core.graphics.Graphic;
 import br.com.etyllica.layer.BufferedLayer;
+import br.com.etyllica.linear.graph.Graph;
+import br.com.etyllica.linear.graph.Node;
+import br.com.etyllica.linear.graph.WeightEdge;
 import br.com.etyllica.motion.core.SearchStrategy;
 import br.com.etyllica.motion.feature.Component;
 import br.com.etyllica.motion.filter.ColorFilter;
 import br.com.etyllica.motion.filter.HardColorFilter;
 import br.com.etyllica.motion.filter.search.FloodFillSearch;
 import br.com.etyllica.motion.filter.search.SoftFloodFillSearch;
+import br.com.etyllica.motion.modifier.ogr.LetterOGRModifier;
 
 public class OGRApplication extends Application {
 
 	private BufferedLayer image;
 
-	private HardColorFilter blackFilter;
+	private ColorFilter blackFilter;
 	
 	private List<Component> blackComponents;
+	private Map<Component, Graph<Integer>> graphs = new HashMap<Component, Graph<Integer>>();
 
 	private Component screen;
 	
 	private Color TEXT_COLOR = new Color(87, 85, 86);
+	
+	private LetterOGRModifier ogr;
 
 	public OGRApplication(int w, int h) {
 		super(w, h);
@@ -39,28 +50,37 @@ public class OGRApplication extends Application {
 
 		loading = 0;
 		//Define the area to search for elements
-		screen = new Component(0, 0, w, h);
-
-		image = new BufferedLayer("ogr/livro.jpg");
-
+		
+		//image = new BufferedLayer("ogr/livro.jpg");
+		//blackFilter = new HardColorFilter(w, h, TEXT_COLOR);
+		
+		image = new BufferedLayer("ogr/pt-sans.png");
+		blackFilter = new ColorFilter(w, h, Color.BLACK);
+		
+		screen = new Component(0, 0, image.getW(), image.getH());
+		
 		loading = 10;
 		//Create the image with elements
 		
 		//Define blue and black filters
-		blackFilter = new HardColorFilter(w, h, TEXT_COLOR);
-		blackFilter.setTolerance(54);
+		//
+		
+		blackFilter.setTolerance(20);
 		//blackFilter.addValidation(new MaxDimensionValidation(20));
 				
 		SearchStrategy floodFill = blackFilter.getSearchStrategy();
 		floodFill.setStep(1);
 		
 		loading = 20;
+		
+		ogr = new LetterOGRModifier();
+		
 		//Filter the image
 		blackComponents = blackFilter.filter(image.getBuffer(), screen);
-
-		loading = 25;
-						
-		loading = 31;
+		
+		for(Component component: blackComponents) {
+			graphs.put(component, ogr.modify(component));
+		}
 				
 		loading = 50;
 
@@ -82,27 +102,44 @@ public class OGRApplication extends Application {
 			g.setStroke(new BasicStroke(3f));
 			g.setColor(Color.RED);
 			g.drawRect(component.getRectangle());
+			
+			g.setColor(Color.BLUE);
+			Graph<Integer> graph = graphs.get(component);
+			if (graph != null)
+				drawGraph(g, graphs.get(component));
 		}
 		
 	}
 	
+	
+	
 	@Override
 	public GUIEvent updateMouse(PointerEvent event) {
 
-		if(event.isButtonDown(MouseButton.MOUSE_BUTTON_LEFT)) {
+		/*if(event.isButtonDown(MouseButton.MOUSE_BUTTON_LEFT)) {
 			Color color = pickColor(event.getX(), event.getY());
 
 			System.out.println(color.getRed());
 			System.out.println(color.getGreen());
 			System.out.println(color.getBlue());
 			System.out.println("---------");
-		}
+		}*/
 
 		return GUIEvent.NONE;
 	}
 	
 	private Color pickColor(int px, int py) {
 		return new Color(image.getBuffer().getRGB(px, py));
+	}
+	
+	private void drawGraph(Graphic g, Graph<Integer> graph) {
+		for(WeightEdge<Integer> edge: graph.getEdges()) {
+			g.drawLine(edge.getOrigin().getPoint(), edge.getDestination().getPoint());
+		}
+		
+		for(Node<Integer> node: graph.getNodes()) {
+			g.fillCircle(node.getPoint(), 4);
+		}
 	}
 
 }
