e46365f3024f99955520730dd5622d729b010c45
Yuri Pourre <yuripourre@gmail.com>
2020-07-28 17:10:48 -0300

    Fix tests

--- src/test/java/com/harium/keel/filter/selection/HSVColorStrategyTest.java
+++ src/test/java/com/harium/keel/filter/selection/HSVColorStrategyTest.java
@@ -4,7 +4,7 @@ import com.harium.etyl.commons.graphics.Color;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class HSLColorStrategyTest {
+public class HSVColorStrategyTest {
 
     private static final float EPSILON = 0.1f;
 
@@ -12,13 +12,13 @@ public class HSLColorStrategyTest {
     public void testConstructor() {
         float tolerance = 0.2f;
 
-        HSLColorStrategy strategy = new HSLColorStrategy(Color.RED.getRGB(), tolerance);
+        HSVColorStrategy strategy = new HSVColorStrategy(Color.RED.getRGB(), tolerance);
         Assert.assertEquals(0.8f, strategy.minH, EPSILON);
         Assert.assertEquals(0.2f, strategy.maxH, EPSILON);// maxH < minH is expected since hue is circular
         Assert.assertEquals(0.8f, strategy.minS, EPSILON);
         Assert.assertEquals(1.0f, strategy.maxS, EPSILON);
-        Assert.assertEquals(0.3f, strategy.minL, EPSILON);
-        Assert.assertEquals(0.7f, strategy.maxL, EPSILON);
+        Assert.assertEquals(0.8f, strategy.minV, EPSILON);
+        Assert.assertEquals(1.0f, strategy.maxV, EPSILON);
     }
 
     @Test
@@ -26,13 +26,13 @@ public class HSLColorStrategyTest {
         float sTolerance = 0.2f;
         float lTolerance = 0.3f;
 
-        HSLColorStrategy strategy = new HSLColorStrategy(Color.BLUE.getRGB(), 0.001f, sTolerance, lTolerance);
-        Assert.assertEquals(0.6656667f, strategy.minH, EPSILON);
-        Assert.assertEquals(0.6676667f, strategy.maxH, EPSILON);// This is expected since hue is circular
+        HSVColorStrategy strategy = new HSVColorStrategy(Color.BLUE.getRGB(), 0.001f, sTolerance, lTolerance);
+        Assert.assertEquals(239.999f, strategy.minH, EPSILON);
+        Assert.assertEquals(239.001f, strategy.maxH, EPSILON);// This is expected since hue is circular
         Assert.assertEquals(0.8f, strategy.minS, EPSILON);
         Assert.assertEquals(1.0f, strategy.maxS, EPSILON);
-        Assert.assertEquals(0.1999999f, strategy.minL, EPSILON);
-        Assert.assertEquals(0.8f, strategy.maxL, EPSILON);
+        Assert.assertEquals(0.7f, strategy.minV, EPSILON);
+        Assert.assertEquals(1.0f, strategy.maxV, EPSILON);
     }
 
 }
